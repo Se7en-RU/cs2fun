438ccc76595ba7183929fdf0b658cca4a6e04551
Yury Kudryavtsev <ykudryavtsev@icloud.com>
2023-06-09 20:42:10 +0400

    app structure, bootstrap 5

---
+++ pages/index.vue
@@ -0,0 +1,32 @@
+<template>
+<div>
+    <h1>Welcome to the homepage</h1>
+    <AppAlert>
+    This is an auto-imported component
+    </AppAlert>
+    
+    <!-- Button trigger js modal -->
+    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+      Launch demo modal
+    </button>
+
+    <!-- Modal -->
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body">
+            I am bootstrap modal using JS!
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+          </div>
+        </div>
+      </div>
+    </div>
+    
+</div>
+</template>
