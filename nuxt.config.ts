1354054f874184ecb3ba8f4e9108439ce1bd26ab
Yury Kudryavtsev <ykudryavtsev@icloud.com>
2023-09-24 00:08:59 +0400

    delete base url

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,6 @@
 import svgLoader from 'vite-svg-loader'
 
 export default defineNuxtConfig({
-  app: {
-    baseURL: '/cs2fun/',
-  },
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
   postcss: {
