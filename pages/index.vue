3fb76d0ed670840ef52d9f9337b9537d8c5d90c8
Yury Kudryavtsev <ykudryavtsev@icloud.com>
2023-09-24 23:08:04 +0400

    init index

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-<main class="relative min-h-screen flex flex-col justify-center bg-slate-900 overflow-hidden">
+<main class="relative min-h-screen flex flex-col justify-center bg-violet-950 overflow-hidden">
         <div class="w-full max-w-6xl mx-auto px-4 md:px-6 py-24">
     
             <div class="text-center">
@@ -20,21 +20,25 @@
                 </div>
             
                 <div class="relative">
-                    <h1 class="inline-flex font-extrabold text-5xl md:text-6xl bg-clip-text text-transparent bg-gradient-to-r from-slate-200/60 via-slate-200 to-slate-200/60 pb-4">Interactive Particle Animation</h1>
+                  <video class="mx-auto mb-7" preload="none" loop autoplay muted width="150px">
+                    <source src="/logo/logo.webm" type="video/mp4">
+                    <source src="/logo/logo.mp4" type="video/mp4">
+                </video>
+                    <h1 class="inline-flex font-extrabold text-5xl md:text-6xl bg-clip-text text-transparent bg-gradient-to-r from-slate-200/60 via-slate-200 to-slate-200/60 pb-4">CSGO.SU</h1>
                     <div class="max-w-3xl mx-auto mb-8">
-                        <p class="text-lg text-slate-400">Our landing page template works on all devices, so you only have to set it up once, and get beautiful results forever.</p>
+                        <p class="text-lg text-slate-400">Добро пожаловать на сервер Шизофрения</p>
                     </div>
                     <div class="inline-flex justify-center space-x-4">
                         <div>
-                            <a class="inline-flex justify-center whitespace-nowrap rounded-lg bg-indigo-500 px-3.5 py-2.5 text-sm font-medium text-white shadow-sm shadow-indigo-950/10 hover:bg-indigo-600 focus-visible:outline-none focus-visible:ring focus-visible:ring-indigo-300 dark:focus-visible:ring-slate-600 transition-colors duration-150 group" href="#0">
-                                Get Started <span class="tracking-normal text-indigo-300 group-hover:translate-x-0.5 transition-transform duration-150 ease-in-out ml-1">-&gt;</span>
+                            <a class="inline-flex justify-center whitespace-nowrap rounded-lg bg-discord px-3.5 py-2.5 text-sm font-medium text-white shadow-sm shadow-indigo-950/10 hover:bg-indigo-600 focus-visible:outline-none focus-visible:ring focus-visible:ring-indigo-300 dark:focus-visible:ring-slate-600 transition-colors duration-150 group" href="https://discord.com/invite/yH8NRqM">
+                                Наш Discord <span class="tracking-normal text-indigo-300 group-hover:translate-x-0.5 transition-transform duration-150 ease-in-out ml-1"></span>
                             </a>
                         </div>
-                        <div>
+                        <!-- <div>
                             <a class="inline-flex justify-center whitespace-nowrap rounded-lg bg-slate-700 px-3.5 py-2.5 text-sm font-medium text-white shadow-sm shadow-indigo-950/10 hover:bg-slate-600 focus-visible:outline-none focus-visible:ring focus-visible:ring-indigo-300 dark:focus-visible:ring-slate-600 transition-colors duration-150" href="#0">
                                 Read the docs
                             </a>
-                        </div>
+                        </div> -->
                     </div>
                 </div>
             </div>
@@ -44,6 +48,9 @@
 </template>
 <script setup lang="ts">
 import Shape from '../assets/svg/shape.svg?component'
+useHead({
+  title: 'CSGO.SU',
+}),
 useHead({
 script: [
   {
